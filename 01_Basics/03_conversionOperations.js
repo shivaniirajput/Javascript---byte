40580b90df6bf68b88f07b0bd915432b5f2361fb
Shivani Rajput <114238450+shivaniirajput@users.noreply.github.com>
2024-05-28 12:11:33 +0000

    conversion operations file added

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -1,25 +1,59 @@
-let score = "33"
+// let score = "33"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
 
-//"33" => 33
-//"33abs" => NaN => not a number
-//true => 1 ; false => 0
-//undefine => NaN
-//null => 0
+// //"33" => 33
+// //"33abs" => NaN => not a number
+// //true => 1 ; false => 0
+// //undefine => NaN
+// //null => 0
 
-let isLoggedIn = 1;
-let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// let isLoggedIn = 1;
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
 
-//1 => true; 0 => false
-//"" => false
-//"shivani" => true
+// //1 => true; 0 => false
+// //"" => false
+// //"shivani" => true
 
-let someNumber = 33;
-let stringNumber = string(someNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33;
+// let stringNumber = string(someNumber);
+// console.log(typeof stringNumber);
+
+//*********************operations*******************//
+
+let val = 3;
+let negVal = -val;
+//console.log(negVal);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello";
+let str2 = " shivani";
+let str3 = str1 + str2; 
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log(+true);           //it will print 1 bcoz true is boolean itself but it will convert because of +
+console.log(+"");             //it will print 0
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100;
+gameCounter++;                //prefix postfix concept
+console.log(gameCounter);
