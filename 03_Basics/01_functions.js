6910bbac085fb8e2ecd3982576c311540a040b23
Shivani Rajput <114238450+shivaniirajput@users.noreply.github.com>
2024-07-03 04:22:47 +0000

    objects in function and array in function

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -34,3 +34,41 @@ function loginUserMessage(username){
 //console.log(loginUserMessage("Shivani"));
 console.log(loginUserMessage())
 
+// function calculateCartPrice(...num1){
+//     return num1
+// }
+
+// console.log(calculateCartPrice(200,400,500))            //rest operator
+
+function calculateCartPrice(val1, val2, ...num1)
+{
+    return num1;
+}
+
+//console.log(calculateCartPrice(100, 200, 3000, 3000, 234, 345, 458));
+
+// const user = {
+//     username: "Shivani",
+//     price: 199
+// }
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+//handleObject(user);
+handleObject(
+    {
+    username: "Shivani",
+    price: 199
+}
+)
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray)
+{
+    return getArray[1];
+}
+
+console.log(returnSecondValue(myNewArray));
