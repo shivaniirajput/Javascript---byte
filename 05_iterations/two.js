8c73186731c085b4decb769537088d9c69333c1a
Shivani Rajput <114238450+shivaniirajput@users.noreply.github.com>
2024-07-04 05:22:56 +0000

    while loop and do while loop done

---
+++ 05_iterations/two.js
@@ -0,0 +1,25 @@
+//while
+
+let i = 0
+while (i <= 10) {
+    //console.log(`value of index is ${i}`);
+    i = i + 2;
+}
+
+let myArray = ['flash', 'batman', 'superman']
+
+let arr = 0;
+while(arr < myArray.length){
+    //console.log(`value is ${myArray[arr]}`);
+    arr = arr + 1;
+}
+
+
+//do while
+
+let score = 11
+
+do {
+    //console.log(`score is ${score}`);
+    score ++;
+} while (score <= 10);
