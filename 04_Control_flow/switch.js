65ba8a01a6527c6283bac7227fc3a2413c5afee8
Shivani Rajput <114238450+shivaniirajput@users.noreply.github.com>
2024-07-04 03:37:39 +0000

    switch case statements done in control flow

---
+++ 04_Control_flow/switch.js
@@ -0,0 +1,55 @@
+// switch(key){
+//     case value:
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 3
+
+// switch (month) {
+//     case 1:
+//         console.log("january")
+//         break;
+
+//     case 2:
+//         console.log("feb")
+//         break;
+
+//     case 3:
+//         console.log("march")
+//         break;
+
+//     case 4:
+//         console.log("april")
+//         break;
+
+//     default:
+//         console.log("default condition match")
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("january")
+        break;
+
+    case "feb":
+        console.log("feb")
+        break;
+
+    case "march":
+        console.log("march")
+        break;
+
+    case "april":
+        console.log("april")
+        break;
+
+    default:
+        console.log("default condition match")
+        break;
+}
